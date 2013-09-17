f5aed5f841fef334a7e34ab849ca165461178208
Simon Rasmussen <zetlon@gmail.com>
2013-09-17 23:17:06 +0200

    Started preliminary work on CRTP and dongle communication

---
+++ CrazySharpLib/CRTPPackets.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CrazySharpLib
+{
+    /// <summary>
+    /// The port used to describe the target on the crazyflie
+    /// </summary>
+    enum PortAllocation : byte
+    {
+        /// <summary>
+        /// Read console text that is printed to the console on the Crazyflie using consoleprintf 
+        /// </summary>
+        Console = 0x0,
+        /// <summary>
+        /// Get/set parameters from the Crazyflie. Parameters are defined using a macro in the Crazyflie source-code
+        /// </summary>
+        Parameters = 0x2,
+        /// <summary>
+        /// Sending control set-points for the roll/pitch/yaw/thrust regulators
+        /// </summary>
+        Commander = 0x3,
+        /// <summary>
+        /// Set up log blocks with variables that will be sent back to the Crazyflie at a specified period. Log variables are defined using a macro in the Crazyflie source-code
+        /// </summary>
+        Log = 0x5,
+        /// <summary>
+        /// Used to control and query the communication link
+        /// </summary>
+        LinkLayer = 0x15
+    }
+
+    public abstract class CRTPPacket
+    {
+        private readonly byte _channel;
+        private readonly PortAllocation _port;
+
+        internal CRTPPacket(byte channel, PortAllocation port)
+        {
+            _channel = channel;
+            _port = port;
+        }
+
+        /// <summary>
+        /// http://wiki.bitcraze.se/projects:crazyflie:firmware:comm_protocol#packet_structure
+        /// </summary>
+        /// <returns>A byte combiantion of the port and channel</returns>
+        protected byte GetHeaderByte()
+        {
+            int val = (((int)_port) << 4) | (_channel & 0x3);
+            return (byte) val;
+        }
+
+        public abstract byte[] CreatePacket();
+    }
+
+
+    public class CRTPCommand : CRTPPacket
+    {
+        internal CRTPCommand(byte channel) : base(channel, PortAllocation.Commander)
+        {
+        }
+
+        public float Roll { get; set; }
+        public float Pitch { get; set; }
+        public float Yaw { get; set; }
+        public UInt16 Thrust { get; set; }
+
+        public override byte[] CreatePacket()
+        {
+            var rollBytes = BitConverter.GetBytes(Roll);
+            var pitchBytes = BitConverter.GetBytes(Pitch);
+            var yawBytes = BitConverter.GetBytes(Yaw);
+            var thrustBytes = BitConverter.GetBytes(Thrust);
+
+
+            var bytes = new byte[14];
+            bytes[0] = GetHeaderByte();
+
+            for (int i = 0; i < 4; i++)
+            {
+                //insert bytes in little endian format like done here: https://bitbucket.org/bitcraze/crazyflie-pc-client/src/2cf63fad1090d21bdd488756ae91954ea07fb576/lib/cflib/crazyflie/commander.py?at=default#cl-73
+                bytes[i] = rollBytes[i];
+                bytes[i + 4] = yawBytes[i];
+                bytes[i + 8] = pitchBytes[i];
+            }
+
+            bytes[12] = thrustBytes[0];
+            bytes[13] = thrustBytes[1];
+
+            return bytes;
+        }
+    }
+
+
+}
---
+++ CrazySharpLib/CrazyFlieDongle.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibUsbDotNet;
+using LibUsbDotNet.Main;
+
+
+namespace CrazySharpLib
+{
+    enum CrazyRequest : byte
+    {
+        SetRadioChannel   = 0x01,
+        SetRadioAddress   = 0x02,
+        SetDataRate       = 0x03,
+        SetRadioPower     = 0x04,
+        SetRadioARD       = 0x05,
+        SetRadioARC       = 0x06,
+        EnableAck         = 0x10,
+        SetContCarrier    = 0x20,
+        StartScanChannels = 0x21,
+        /*GetScanChannels = 0x21*/
+        LaunchBootloader  = 0xFF
+    }
+
+
+    public class CrazyFlieDongle
+    {
+        private readonly UsbRegistry _deviceReg;
+        private UsbDevice _device;
+        private byte _radioChannel;
+        private byte[] _radioAdress;
+
+        private CrazyFlieDongle(UsbRegistry deviceReg)
+        {
+            _deviceReg = deviceReg;
+        }
+
+        public void OpenDevice()
+        {
+            if (_deviceReg.Open(out _device) == false)
+            {
+                throw new InvalidOperationException("Unable to open the dongle, was it removed?");
+            }
+        }
+
+        public void CloseDevice()
+        {
+            if(_device == null || _device.IsOpen == false)
+            {
+                throw new InvalidOperationException("Unable to close the device because it has not yet been opened");
+            }
+            
+            if(_device.Close() == false)
+                throw new InvalidOperationException("Unable to close the device for unknown reasons, maybe it was removed");
+        }
+
+
+        public byte RadioChannel
+        {
+            get { return _radioChannel; }
+            set
+            {
+                if(value > 125)
+                    throw new ArgumentOutOfRangeException("Channel must be between 0 and 125");
+
+                _radioChannel = value;
+            }
+        }
+
+        public byte[] RadioAdress
+        {
+            get{ return _radioAdress; }
+            set
+            {
+                if(value.Length != 5)
+                    throw new ArgumentOutOfRangeException("value", "Address must be 5 bytes long");
+
+                _radioAdress = value;
+            }
+        }
+
+        public byte DataRate { get; set; }
+
+
+
+        private void SendVendorSetup(byte request, byte value, int index = 0, byte[] data = null)
+        {
+            
+
+
+        }
+
+
+
+        #region Static methods
+
+        /// <summary>
+        /// Get all crazyflie dongles connected to the computer.
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<CrazyFlieDongle> GetDongles()
+        {
+            foreach (UsbRegistry dev in UsbDevice.AllDevices)
+            {
+                if(DonglePredicate(dev))
+                {
+                    yield return new CrazyFlieDongle(dev);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Matches the VID and PID according to: http://wiki.bitcraze.se/projects:crazyradio:protocol?s[]=pid#usb_protocol
+        /// </summary>
+        private static bool DonglePredicate(UsbRegistry reg)
+        {
+            return reg.Pid == 0x7777 && reg.Vid == 0x1915;
+        }
+
+        #endregion
+    }
+}
